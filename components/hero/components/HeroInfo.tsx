67aeb2cbafb4a2a7e08d79be6d6367b491fca5a5
jaminhoood <princeobj5@gmail.com>
2024-05-22 12:03:06 +0100

    revamp: change CV link

--- components/hero/components/HeroInfo.tsx
+++ components/hero/components/HeroInfo.tsx
@@ -8,7 +8,7 @@ const HeroInfo = () => {
 				<h1 className="text-5xl md:text-7xl leading-10 font-bold capitalize">Benjamin Ovu</h1>
 				<h6 className="text-lg font-normal capitalize mt-4 border-l-2 border-r-2 md:border-r-0 border-green-500 inline-block py-2 px-4">A Software Engineer</h6>
 				<Link
-					href="https://drive.google.com/file/d/1oH6k5VT7yQ1DUQ_s0qCV7Jbw8gfPMQJs/view?usp=drive_link"
+					href="https://drive.google.com/file/d/1psbzSOhUS2ITgaPonwRGT_5rGIffWdHp/view?usp=sharing"
 					target="_blank">
 					<button className="py-2 px-6 mt-4 bg-green-500 text-gray-50 border-2 border-green-500 hover:bg-transparent hover:text-green-500 transition-all ease-in-out duration-500 block">Download CV.</button>
 				</Link>
